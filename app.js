a05e7464990a35221afe14d3c116a453cb25beaf
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-09-17 21:59:07 -0300

    Criando home page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
     const mongoose = require('mongoose');
     const session = require('express-session');
     const flash = require('connect-flash');
+    require("./models/Postagem");
+    const Postagem = mongoose.model("postagens");
 
 //Configurações
     //Sessões
@@ -44,7 +46,16 @@
 //Rotas
     app.use('/admin', admin);
     app.get('/', (req,res) => {
-        res.send('Página principal');
+        Postagem.find().populate("categoria").sort({data:'desc'}).then((postagens) => {
+            res.render('index', {postagens: postagens});
+        }).catch((err) => {
+            req.flash("error_msg", "Erro ao carregar as postagens");
+            res.redirect("/404");
+        })
+    })
+
+    app.get("/404", (req,res) => {
+        res.send("Erro 404.");
     })
     app.get('/posts', (req,res) => {
         res.send('Página de posts');
