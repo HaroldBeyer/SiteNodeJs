b359baa3c49b60036877a0d19303695a75dbeb0a
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-09-19 15:25:30 -0300

    Logout

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -80,4 +80,9 @@ router.post('/registro', (req,res) => {
             failureFlash: true
         })(req,res,next)
     })
+    router.get('/logout', (req,res) => {
+        req.logout();
+        req.flash('success_msg', "Deslogado com sucesso");
+        res.redirect('/');
+    })
 module.exports = router;
